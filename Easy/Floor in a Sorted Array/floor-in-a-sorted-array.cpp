c4ce1e1c17e4f554f1d66f2488796963209f0f3a
Esha Mitra <72601896+esha20@users.noreply.github.com>
2023-01-31 18:13:36 +0530

    Updated Solution - GfG to GitHub

--- Easy/Floor in a Sorted Array/floor-in-a-sorted-array.cpp
+++ Easy/Floor in a Sorted Array/floor-in-a-sorted-array.cpp
@@ -9,27 +9,57 @@ class Solution{
     // Function to find floor of x
     // n: size of vector
     // x: element whose floor is to find
-    int findFloor(vector<long long> v, long long n, long long x){
+    
+    //iterative approach
+    // int findFloor(vector<long long> v, long long n, long long x){
         
-        // Your code here
-        long long start =0, end = n-1;
-        long long mid = start+(end-start)/2;
-        long long ans =-1;
-        while(start<=end){
-            if(v[mid]==x)
-                return mid;
-            else if(v[mid]>x)
-            {
-               // ans = mid;
-                end = mid-1;
-            }
-            else{
-                ans = mid;
-                start = mid+1;
-            }
-           mid = start+(end-start)/2; 
+    //     // Your code here
+    //     long long start =0, end = n-1;
+    //     long long mid = start+(end-start)/2;
+    //     long long ans =-1;
+    //     while(start<=end){
+    //         if(v[mid]==x)
+    //             return mid;
+    //         else if(v[mid]>x)
+    //         {
+    //           // ans = mid;
+    //             end = mid-1;
+    //         }
+    //         else{
+    //             ans = mid;
+    //             start = mid+1;
+    //         }
+    //       mid = start+(end-start)/2; 
+    //     }
+    //   return ans; 
+    // }
+    
+    //recurrsive approach
+    
+    void solve(vector<long long> v, long long s,long long e, long long x, int &ans){
+        if(s>e)
+            return;
+        
+        int mid = s+(e-s)/2;
+        
+        if(v[mid]==x)
+            ans = mid;
+            
+        else if(v[mid]>x)
+            solve(v,s,mid-1,x,ans);
+            
+        else
+        {
+            ans = mid;
+            solve(v,mid+1,e,x,ans);
         }
-       return ans; 
+            
+    }
+    
+    int findFloor(vector<long long> v, long long n, long long x){
+        int ans = -1;
+        solve(v, 0 , n-1,x, ans);
+        return ans;
     }
 };
 
