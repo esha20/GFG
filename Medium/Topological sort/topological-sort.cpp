60e63fff0c9ee380d79af1ce220a650150e2c330
Esha Mitra <72601896+esha20@users.noreply.github.com>
2023-04-22 13:16:18 +0530

    Updated Solution - GfG to GitHub

--- Medium/Topological sort/topological-sort.cpp
+++ Medium/Topological sort/topological-sort.cpp
@@ -5,37 +5,76 @@ using namespace std;
 // } Driver Code Ends
 class Solution
 {
-	public:
-	
-	void dfs(int node,vector<int> adj[],unordered_map <int, bool> &visited,stack<int> &s){
-	    visited[node]=1;
-	    for(auto i : adj[node]){
-	        if(!visited[i]){
-	            dfs(i,adj,visited,s);
-	        }
-	    }
-	    s.push(node);
-	}
-	//Function to return list containing vertices in Topological order. 
-	vector<int> topoSort(int V, vector<int> adj[]) 
-	{
-	    // code here
-	    stack <int> s;
-	    unordered_map <int, bool> visited;
+// 	public:
+// 	void dfs(int node,vector<int> adj[],unordered_map <int, bool> &visited,stack<int> &s){
+// 	    visited[node]=1;
+// 	    for(auto i : adj[node]){
+// 	        if(!visited[i]){
+// 	            dfs(i,adj,visited,s);
+// 	        }
+// 	    }
+// 	    s.push(node);
+// 	}
+// 	//Function to return list containing vertices in Topological order. 
+// 	vector<int> topoSort(int V, vector<int> adj[]) 
+// 	{
+// 	    // code here
+// 	    stack <int> s;
+// 	    unordered_map <int, bool> visited;
 	    
-	    for(int i=0;i<V;i++){
-	        if(!visited[i]){
-	            dfs(i,adj,visited,s);
-	        }
-	    }
-	    vector<int> ans;
-	    while(!s.empty()){
-	       // int x = s.pop();
-	        ans.push_back(s.top());
-	        s.pop();
-	    }
-	    return ans;
-	}
+// 	    for(int i=0;i<V;i++){
+// 	        if(!visited[i]){
+// 	            dfs(i,adj,visited,s);
+// 	        }
+// 	    }
+// 	    vector<int> ans;
+// 	    while(!s.empty()){
+// 	       // int x = s.pop();
+// 	        ans.push_back(s.top());
+// 	        s.pop();
+// 	    }
+// 	    return ans;
+// 	}
+
+public:
+
+vector<int> topoSort(int v, vector<int> adj[]) 
+{
+    vector<int> ans;
+    queue<int> q;
+    vector<int> indegree(v);
+
+    //indegree vector create kar lo
+    for(int i=0;i<v;i++){
+        for(auto j : adj[i]){
+            indegree[j]++;
+        }
+    }
+    
+    //0 indegree walo ko queue mei add kar do.
+    for(int i=0;i<v;i++){
+        if(indegree[i]==0){
+            q.push(i);
+        }
+    }
+    
+    //bfs chala do bakiyo k liye
+    while(!q.empty()){
+        
+        int frontnode = q.front();
+        q.pop();
+        
+        for(auto i: adj[frontnode]){
+            indegree[i]--;
+            if(indegree[i]==0){
+                q.push(i);
+            }
+        }
+        
+        ans.push_back(frontnode);
+    }
+    return ans;
+}
 };
 
 //{ Driver Code Starts.
