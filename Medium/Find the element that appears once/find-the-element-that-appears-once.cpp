cf5c51beb857253e1d5e49d1805887ad5954da6f
Esha Mitra <72601896+esha20@users.noreply.github.com>
2023-04-28 16:17:09 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Find the element that appears once/find-the-element-that-appears-once.cpp
@@ -0,0 +1,41 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// } Driver Code Ends
+//User function template for C++
+class Solution{
+public:	
+	int search(int arr[], int n){
+	    //code
+	    int i=0;
+	    while(i+1<n){
+	        if(arr[i]!=arr[i+1])    return arr[i];
+	        i+=2;
+	    }
+	    return arr[n-1];
+	}
+};
+
+//{ Driver Code Starts.
+
+// Driver program
+int main()
+{
+    int t,len;
+    cin>>t;
+    while(t--)
+    {
+        cin>>len;
+        int arr[len];
+        for(int i=0;i<len;i++){
+            cin>>arr[i];
+        }
+        Solution ob;
+        cout<<ob.search(arr, len)<<'\n';
+    }
+    return 0;
+}
+
+// } Driver Code Ends
